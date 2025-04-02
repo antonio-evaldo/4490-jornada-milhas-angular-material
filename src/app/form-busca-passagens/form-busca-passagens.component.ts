18990cf58032941f9454bc824f6b93871f6229ef
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-04-02 17:13:48 -0300

    Vídeo 4.2 - Solucionando o desafio: adicionando primeiro input

--- src/app/form-busca-passagens/form-busca-passagens.component.ts
+++ src/app/form-busca-passagens/form-busca-passagens.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-form-busca-passagens',
-  imports: [ MatButtonToggleModule ],
+  imports: [MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './form-busca-passagens.component.html',
   styleUrl: './form-busca-passagens.component.scss'
 })
