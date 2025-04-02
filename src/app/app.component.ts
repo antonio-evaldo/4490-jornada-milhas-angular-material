8c5640bcfb544f021fa9a095a036a9c55ae39e71
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-04-02 15:41:26 -0300

    Vídeo 2.3 - Solucionando o desafio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ MatButtonModule ],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
