7cc68fc43bfc61718b367b39410d0f816bde61d7
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-04-02 16:05:29 -0300

    Mão na massa - Criando Banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { BannerComponent } from "./banner/banner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
