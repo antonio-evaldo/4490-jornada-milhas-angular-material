37f96b39a05f00e109ccb717d760bcb860f3465b
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-04-02 17:33:21 -0300

    Vídeo 4.3 - Finalizando os inputs

--- src/app/form-busca-passagens/form-busca-passagens.component.ts
+++ src/app/form-busca-passagens/form-busca-passagens.component.ts
@@ -3,10 +3,12 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-form-busca-passagens',
-  imports: [MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './form-busca-passagens.component.html',
   styleUrl: './form-busca-passagens.component.scss'
 })
